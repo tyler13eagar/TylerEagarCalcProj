f68b1937ec44c3d9fe040e350685af10fcf586c9
tyler13eagar <tyler13eagar@gmail.com>
2022-02-25 01:25:06 -0700

    Add files via upload

---
+++ Calculator.feature
@@ -0,0 +1,69 @@
+﻿Feature: Calculator
+![Calculator](https://specflow.org/wp-content/uploads/2020/09/calculator.png)
+Simple calculator for adding **two** numbers
+
+Link to a feature: [Calculator](TylerEagarCalcProj.Specs/Features/Calculator.feature)
+***Further read***: **[Learn more about how to generate Living Documentation](https://docs.specflow.org/projects/specflow-livingdoc/en/latest/LivingDocGenerator/Generating-Documentation.html)**
+
+@mytag
+Scenario: Add two numbers
+	Given the first number is 50
+	And the second number is 70
+	When the two numbers are added
+	Then the result should be 120
+
+Scenario: Add two other numbers
+	Given the first number is 50
+	And the second number is 60
+	When the two numbers are added
+	Then the result should be 110
+
+Scenario: Subtract a small number from a bigger number
+	Given the first number is 100
+	And the second number is 75
+	When the two numbers are subtracted
+	Then the result should be 25
+
+Scenario: Subtract a bigger number from a smaller number
+	Given the first number is 10
+	And the second number is 75
+	When the two numbers are subtracted
+	Then the result should be -65
+
+Scenario: Multiply two positive numbers
+	Given the first number is 100
+	And the second number is 75
+	When the two numbers are multiplied
+	Then the result should be 7500
+
+ 
+Scenario: Heber assignment 1
+	Given the first number is 100
+	When operation + is done to the number 50
+	And operation + is done to the number 80
+	Then the result should be 230
+
+	
+Scenario: Heber assignment 2
+	Given the first number is 100
+	When operation + is done to the number 50
+	And operation + is done to the number 80
+	And operation + is done to the number 80
+	Then the result should be 310
+
+Scenario: Heber assingment 3
+	Given the first number is 100
+	When operation + is done to the number 50
+	And operation + is done to the number 80
+	And operation + is done to the number 80
+	And operation / is done to the number 10
+	Then the result should be 31
+
+ 
+Scenario: Heber assignment 4
+	Given the first number is 100
+	When operation / is done to the number 50
+	And operation - is done to the number 2
+	And operation + is done to the number 10
+	And operation % is done to the number 3
+	Then the result should be 1
---
+++ CalculatorStepDefinitions.cs
@@ -0,0 +1,87 @@
+﻿using TechTalk.SpecFlow;
+using FluentAssertions;
+
+namespace TylerEagarCalcProj.Specs.Steps
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        private readonly ScenarioContext _scenarioContext;
+
+        private readonly Calculator _calculator = new Calculator();
+
+        private int _result;
+
+        public CalculatorStepDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given("the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int number)
+        {
+            _calculator.FirstNumber = number;
+        }
+
+        [Given("the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int number)
+        {
+            _calculator.SecondNumber = number;
+        }
+
+        //add
+        [When("the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            _result = _calculator.Add();
+        }
+
+        //subtract
+        [When("the two numbers are subtracted")]
+        public void WhenTheTwoNumbersAreSubtracted()
+        {
+            _result = _calculator.Sub();
+        }
+
+        //multiplied
+        [When("the two numbers are multiplied")]
+        public void WhenTheTwoNumbersAreMultiplied()
+        {
+            _result = _calculator.Mult();
+        }
+
+        
+        [When(@"operation \+ is done to the number (.*)")]
+        public void WhenOperationAddIsDoneToTheNumber(int number)
+        {
+            _result = _calculator + number;
+        }
+
+        [When(@"operation \- is done to the number (.*)")]
+        public void WhenOperationSubtractIsDoneToTheNumber(int number)
+        {
+            _result = _calculator - number;
+        }
+
+        [When(@"operation \/ is done to the number (.*)")]
+        public void WhenOperationDivideIsDoneToTheNumber(int number)
+        {
+            _result = _calculator / number;
+        }
+
+        [When(@"operation \% is done to the number (.*)")]
+        public void WhenOperationModulasIsDoneToTheNumber(int number)
+        {
+            _result = _calculator % number;
+        }
+
+        [Then("the result should be (.*)")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            _result.Should().Be(result);
+        }
+    }
+}
