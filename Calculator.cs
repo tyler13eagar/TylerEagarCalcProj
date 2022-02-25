578864c68b877e3292fdedde8d6eae58740c5fbb
Tyler Eagar <tyler13eagar@gmail.com>
2022-02-25 01:06:34 -0700

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace TylerEagarCalcProj
+{
+    public class Calculator
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+
+        public int Add()
+        {
+            return FirstNumber + SecondNumber;
+        }
+
+        public int Sub()
+        {
+            return FirstNumber - SecondNumber;
+        }
+
+        public int Mult()
+        {
+            return FirstNumber * SecondNumber;
+        }
+
+        public static int operator +(Calculator calc, int number)
+        {
+            calc.FirstNumber += number;
+            return calc.FirstNumber;
+        }
+
+        public static int operator -(Calculator calc, int number)
+        {
+            calc.FirstNumber -= number;
+            return calc.FirstNumber;
+        }
+
+        public static int operator /(Calculator calc, int number)
+        {
+            calc.FirstNumber /= number;
+            return calc.FirstNumber;
+        }
+
+        public static int operator %(Calculator calc, int number)
+        {
+            calc.FirstNumber %= number;
+            return calc.FirstNumber;
+        }
+    }
+}
